9bbe8157b9143b42b556cc49875483363e923851
fischermatte <fischermatte@posteo.net>
2021-02-28 22:31:15 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 8092;
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        res.write(data);
+        res.end();
+    });
+}).listen(port);
+
+console.log("Server running at http://localhost:%d", port);
