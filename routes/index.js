d355f555b04f25cf0f7578c43fb77edc03525e87
fischermatte <fischermatte@posteo.net>
2021-02-28 23:23:24 +0100

    added welcome msg

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const headers = JSON.stringify(req.headers)
-  res.render('index', { title: 'Express', headers });
+  const principal = req.headers["x-ms-client-principal-name"]
+  res.render('index', { title: 'Express', headers, principal });
 });
 
 module.exports = router;
